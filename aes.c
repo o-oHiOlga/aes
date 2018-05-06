3ffd119efe4e531d566512f38d5da9b9a9d13d80
Olga <olga@MacBook-Pro-de-Olga.local>
2018-05-06 17:07:00 -0500

    Agregando tablas s-box y invs-box

--- aes.c
+++ aes.c
@@ -3,47 +3,48 @@
 #include <string.h>
 #include "aes.h"
 
-word rotWord(word key){
-  word rot = key << 8;
-  key>>=24;
-  rot= rot|key;
-  //printf("KeyRot: %x\n",key&0xffffffff);
-  return rot;
-}
+
 
 void cipher (byte in[4*Nb], byte out[4*Nb],word w[Nb*(Nr+1)]){
-  byte state[4*Nb];
-  //state=in;
+
 }
+byte subWord(word temp){
+  int row,column;
+  byte postSbox;
+  row = temp & 0xffff0000;
+  column = temp & 0x0000ffff;
+  postSbox=Sbox[row][column];
+  return postSbox;
+}
+
+void keyExpansion(byte key[4*Nk], word *w[Nb*(Nr+1)]){
+  word temp;
+  int i=0;
+  while (i<Nk){
+    w[i]=word(key[4*i],key[4*i+1],key[4*i+2],key[4*i+3]);
+    i=i+1;
+  }
+  i=Nk;
+  while(i<Nb*(Nr+1)){
+    temp = w[i-1];
+    if(i%Nk==0){
+      temp=subWord(rotWord(temp))^rcon[i/Nk];
+    }else if(Nk>6 && i%Nk==4){
+      temp=subWord(temp);
+    }
+    w[i]=w[i-Nk]^temp;
+    i=i+1;
+  }
+}
+
 
 
 int main (int argc,char *argv[]){
   byte input[4*Nb]={0x32,0x43,0xf6,0xa8,0x88,0x5a,0x30,0x8d,0x31,0x31,0x98,0xa2,0xe0,0x37,0x07,0x34};
   word key[4] = {0x2b7e1516,0x28aed2a6,0xabf71588,0x09cf4f3c};
+  word keyExp[Nb*(Nr+1)];
 
-  printf("%x\n",key[0]&0xffffffff);
-
-  word rotation = rotWord(key[0]);
-  printf("Rotation: %x\n",rotation&0xffffffff);
-
-  byte in[4][4];
-  int i,j,k=0;
-  for(i=0;i<4;i++){
-    for(j=0;j<4;j++){
-      in[j][i]=input[k];
-      k++;
-    }
-  }
-  //char ch = 0xC0;
-//printf("%x", ch & 0xff);
-
-  for(i=0;i<4;i++){
-    for(j=0;j<4;j++)
-      printf("%x ",in[i][j]&0xff);
-    printf("\n" );
-  }
-
-
+  keyExpansion(key[0],&keyExp);
 
 return 0;
 }
